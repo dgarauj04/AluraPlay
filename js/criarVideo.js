0ad63e6beaf3e75c128ca65a7619c68583a479c5
Douglas Araujo <douglasdab10@gmail.com>
2024-12-27 02:22:24 -0300

    Update criarVideo.js

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -4,17 +4,36 @@ const formulario = document.querySelector("[data-formulario]");
 async function criarVideo(evento) {
     evento.preventDefault();
 
-    const imagem = document.querySelector("[data-imagem]").value;
+    const imagemUrl = document.querySelector("[data-imagem-url]").value;
+    const imagemUpload = document.querySelector("[data-imagem-upload]");
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
+
+    let imagem;
+
+if (imagemUpload.files.length > 0) {
+    const file = imagemUpload.files[0];
+    const reader = new FileReader();
+
+    reader.onloadend = function() {
+        imagem = reader.result; 
+        enviarVideo(titulo, descricao, url, imagem);
+    };
+    reader.readAsDataURL(file); 
+} else {
+    imagem = imagemUrl; 
+    enviarVideo(titulo, descricao, url, imagem);
+   }
+}
+
+async function enviarVideo(titulo, descricao, url, imagem) {
 try {
     await conectaApi.criaVideo(titulo, descricao, url, imagem);
-
     window.location.href = "../pages/envio-concluido.html";
 } catch (e) {
-alert(e);
-}
+    alert(e);
+   }
 }
     
-formulario.addEventListener("submit", evento => criarVideo(evento))
+formulario.addEventListener("submit", evento => criarVideo(evento));
